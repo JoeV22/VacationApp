6ac04cf4c1eb0a1fbd84c00d6042e3992cc2939a
JoeV22 <79062669+JoeV22@users.noreply.github.com>
2022-09-15 15:58:42 -0400

    Initial Commit

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Div, ScrollView, Logo,
+  ImageBackground,
+  TouchableOpacity, Pressable } from 'react-native';
+import { pattern, buttons } from './Stylesheets/styles'   
 
 export default function App() {
   return (
+    <ScrollView>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <TouchableOpacity>
+    <View style={styles.HeaderContainer}>
+      <Text style={styles.Header}>
+      Vacation Ex
+      </Text>
+    </View>
+    </TouchableOpacity>
+      <Image
+      source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/James_Franco_4%2C_2013.jpg' }}
+      style={{ width: "25%", height: "15%", radius: "50%", marginBottom:"100%",}}
+    />
       <StatusBar style="auto" />
     </View>
+    </ScrollView>
   );
 }
 
@@ -17,4 +32,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  HeaderContainer: {
+    flex: 1,
+    color: '#0000FF',
+    marginTop: "65%",
+
+  },
+  Header: {
+    flex: 1,
+    color: '#0000FF',
+    marginTop: "25%",
+    fontWeigth:"bold",
+    fontSize: "30",
+    marginRight:"40%",
+    textAlignVertical: "top"
+
+  },
+  
 });
---
+++ Stylesheets/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+  
+const pattern = StyleSheet.create({ 
+
+
+})
+ 
+const buttons = StyleSheet.create({
+
+})
+ 
+export { pattern, buttons };      
